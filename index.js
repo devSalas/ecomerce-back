e0ad2f469149be89006ff454a4caea2b34b0bc26
esteban salas <esalassulca@gmail.com>
2023-01-13 14:29:11 -0500

    mover archivo index a la carpeta raiz

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ app.get("/",(req,res)=>{
 
   res.send("saludo desde el backend  hola ")
 })
+
   
 app.post("/create-payment-intent", async (req, res) => {
 
@@ -51,9 +52,9 @@ app.post("/create-payment-intent", async (req, res) => {
 
 
 
+const PORT =process.env.PORT || 3000
 
 
-
-app.listen(process.env.PORT, () => {
-  console.log('App listening on port '+process.env.PORT);
+app.listen(PORT, () => {
+  console.log('App listening on port '+ PORT);
 })
